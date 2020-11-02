914ae9ed644a22c62e53b1ef322e01dc3be21e8f
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-02 01:39:45 -0700

    v-6 create the navbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Game from './pages/Game';
 import GameOver from './pages/GameOver';
 import HighScores from './pages/HighScores';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path='/game' component={Game} />
         <Route path='/highScores' component={HighScores} />
