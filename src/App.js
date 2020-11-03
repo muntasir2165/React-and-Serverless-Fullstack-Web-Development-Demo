641b44e5329cc8fc40ac0e953f4bfe438052fc01
Muntasir Biojid <muntasir2165@hotmail.com>
2020-11-02 21:11:21 -0700

    v-8 create base styled components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Game from './pages/Game';
 import GameOver from './pages/GameOver';
 import HighScores from './pages/HighScores';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import { Container } from './styled/Container';
+import { Main } from './styled/Main';
+import Global from './styled/Global';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/game' component={Game} />
-        <Route path='/highScores' component={HighScores} />
-        <Route path='/gameOver' component={GameOver} />
-        <Route path='/' component={Home} />
-      </Switch>
+      <Global />
+      <Main>
+        <Container>
+          <Navbar />
+          <Switch>
+            <Route path='/game' component={Game} />
+            <Route path='/highScores' component={HighScores} />
+            <Route path='/gameOver' component={GameOver} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </Container>
+      </Main>
     </Router>
   );
 }
